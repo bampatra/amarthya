3c743d8f8576a53631851e5138216a8dd3bb57b1
bampatra <s3549111@student.rmit.edu.au>
2021-03-19 11:05:59 +0800

    Customer & vendor v1. Staff v2

---
+++ application/modules/main/views/customer.php
@@ -0,0 +1,274 @@
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Data Staff</h1>
+    <br>
+
+    <button class="btn btn-primary add" style="background: #a50000; color: white; width: 300px;"> Tambah Staff </button>
+    <br> <br>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Daftar Staff</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                    <tr>
+                        <th style="display: none;"> ID </th>
+                        <th> Nama Staff </th>
+                        <th> Posisi </th>
+                    </tr>
+                    </thead>
+                    <tbody id="main-content">
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
+<div class="modal fade" tabindex="-1" role="dialog" id="staff-modal" style="z-index: 5000">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Staff</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="staff-form">
+                    <div class="form-group" >
+                        <label class="col-form-label">Nama Staff</label>
+                        <input type="text" id="nama_staff"  name="nama_staff" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-namastaff">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-form-label">Tanggal Lahir</label>
+                        <input type="date" id="tgl_lahir_staff" name="tgl_lahir_staff" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-tanggallahir">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-form-label">Alamat</label>
+                        <textarea id="alamat_staff" name="alamat_staff" class="form-control form-active-control"> </textarea>
+                        <div class="invalid-feedback invalid-alamat">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group" >
+                        <label class="col-form-label">Posisi</label>
+                        <select id="id_posisi" name="id_posisi" class="form-control form-active-control selectpicker" data-live-search="true">
+                            <option value="none"> -- Pilih Posisi -- </option>
+                            <?php foreach ($posisi_list as $posisi) { ?>
+                                <option value="<?php echo $posisi->id_posisi; ?>">
+                                    <?php echo $posisi->nama_posisi; ?>
+                                </option>
+
+
+                            <?php } ?>
+                        </select>
+                        <div class="invalid-feedback invalid-posisi">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group" >
+                        <label class="col-form-label">Salary per 2 Minggu</label>
+                        <input type="number" id="salary"  name="salary" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-salary">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group" >
+                        <label class="col-form-label">No. Rekening</label>
+                        <input type="number" id="no_rek_staff"  name="no_rek_staff" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-norek">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group" >
+                        <label class="col-form-label">Nama Bank</label>
+                        <input type="text" id="nama_bank_staff"  name="nama_bank_staff" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-namabank">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-form-label">Tanggal Bergabung</label>
+                        <input type="date" id="tgl_join_staff" name="tgl_join_staff" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-tanggaljoin">Data tidak valid</div>
+                    </div>
+
+                    <input type="hidden" id="id_staff" name="id_staff" val="0">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <div class="modal-button-view-only">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-danger delete">Hapus</button>
+                    <button type="button" class="btn btn-primary edit">Edit</button>
+                </div>
+                <div class="modal-button-save">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary save">Simpan</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<style>
+    tr:hover{
+        cursor: pointer;
+        background: rgba(20,143,143,0.5);
+        transition: background-color 0.15s ease-in-out;
+    }
+
+</style>
+
+<!-- Page level custom scripts -->
+
+<!-- <script src="<?php echo base_url('assets/js/startbootstrap/demo/datatables-demo.js');?>"></script>-->
+
+<script>
+
+    $('#collapseUser').addClass('show');
+    $('#navbar-user').addClass('active');
+
+    get_staff();
+
+    //get all products
+    function get_staff(){
+        $('.loading').css("display", "block");
+        $('.Veil-non-hover').fadeIn();
+        $.ajax({
+            type        : 'GET', // define the type of HTTP verb we want to use (POST for our form)
+            url         : admin_url + 'get_staff', // the url where we want to POST// our data object
+            dataType    : 'json',
+            success     : function(data){
+                html = '';
+                data.forEach(function(data){
+
+                    html += '<tr>'+
+                        '<td style="display: none;">'+ data.id_staff +'</td>';
+
+                    html += ' <td>'+ data.nama_staff +'</td>';
+
+                    html +=  ' <td>'+ data.nama_posisi +'</td>\n'+
+                        '                    </tr>';
+                })
+
+                $('#dataTable').DataTable().destroy();
+                $('#main-content').html(html);
+                $('#dataTable').DataTable({
+                    "lengthChange": false
+                });
+
+                $('.loading').css("display", "none");
+                $('.Veil-non-hover').fadeOut();
+            }
+        })
+    }
+
+
+    $('#dataTable').on( 'click', 'tbody tr', function () {
+        $('.loading').css("display", "block");
+        $('.Veil-non-hover').fadeIn();
+        $('body').addClass('modal-open');
+        id_staff = $('#dataTable').DataTable().row( this ).data()[0];
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : admin_url + 'get_staff_by_id', // the url where we want to POST// our data object
+            dataType    : 'json',
+            data        : {id_staff: id_staff},
+            success     : function(data){
+
+                setTimeout(function() {$('.modal-dialog').scrollTop(0);}, 200);
+                $('#id_staff').val(htmlDecode(data.id_staff));
+                $('#nama_staff').val(htmlDecode(data.nama_staff));
+                $('#tgl_lahir_staff').val(htmlDecode(data.custom_tgl_lahir));
+                $('#alamat_staff').val(htmlDecode(data.alamat_staff));
+                $('#id_posisi').val(htmlDecode(data.id_posisi));
+                $('#salary').val(htmlDecode(data.salary_staff));
+                $('#no_rek_staff').val(htmlDecode(data.no_rek_staff));
+                $('#nama_bank_staff').val(htmlDecode(data.nama_bank_staff));
+                $('#tgl_join_staff').val(htmlDecode(data.custom_tgl_join));
+
+
+                $('.form-active-control').prop('disabled', true);
+
+                $('.modal-button-save').css('display', 'none');
+                $('.modal-button-view-only').css('display', 'block');
+                $('#staff-modal').modal('toggle');
+
+                $('.selectpicker').selectpicker('refresh')
+                $('.loading').css("display", "none");
+                $('.Veil-non-hover').fadeOut();
+            }
+        })
+    });
+
+    $('.add').click(function (e) {
+        e.preventDefault();
+        $('.invalid-feedback').css('display', 'none');
+        $('#id_staff').val(0);
+        $('#id_posisi').val('none');
+        setTimeout(function() {$('.modal-dialog').scrollTop(0);}, 200);
+        $('.modal-button-view-only').css('display', 'none');
+        $('#staff-form').trigger('reset');
+        $('.form-active-control').prop('disabled', false);
+        $('#staff-modal').modal('toggle');
+        $('.modal-button-save').css('display', 'block');
+    })
+
+    $('.edit').click(function(e){
+        $('.invalid-feedback').css('display', 'none');
+        setTimeout(function() {$('.modal-dialog').scrollTop(0);}, 200);
+        $('.modal-button-save').css('display', 'block');
+        $('.modal-button-view-only').css('display', 'none');
+        $('.form-active-control').prop('disabled', false);
+        $('.selectpicker').selectpicker('refresh')
+    })
+
+    $('.save').click(function(e){
+        $('.loading').css("display", "block");
+        $('.Veil-non-hover').fadeIn();
+        $.ajax({
+            type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url: admin_url + 'add_staff', // the url where we want to POST// our data object
+            dataType: 'json',
+            data: $('#staff-form').serialize(),
+            success: function (response) {
+                $('.invalid-feedback').css('display', 'none');
+                if(response.Status == "OK"){
+                    get_staff();
+                    $('#staff-modal').modal('hide');
+                } else if(response.Status == "FORMERROR") {
+                    response.Error.forEach(function(error){
+                        $('.'+ error +'').css('display', 'block');
+                    })
+                } else if(response.Status == "EXIST") {
+                    show_snackbar('Nama Staff sudah terdaftar');
+                } else if(response.Status == "ERROR" ){
+                    show_snackbar(response.Message);
+                }
+
+                $('.loading').css("display", "none");
+                $('.Veil-non-hover').fadeOut();
+            }
+        })
+    })
+
+
+
+</script>
---
+++ application/modules/main/views/vendor.php
@@ -0,0 +1,236 @@
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Data Staff</h1>
+    <br>
+
+    <button class="btn btn-primary add" style="background: #a50000; color: white; width: 300px;"> Tambah Customer </button>
+    <br> <br>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Daftar Customer</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                    <tr>
+                        <th style="display: none;"> ID </th>
+                        <th> Nama Customer </th>
+                    </tr>
+                    </thead>
+                    <tbody id="main-content">
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
+<div class="modal fade" tabindex="-1" role="dialog" id="customer-modal" style="z-index: 5000">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Customer</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="customer-form">
+                    <div class="form-group" >
+                        <label class="col-form-label">Nama Customer</label>
+                        <input type="text" id="nama_customer"  name="nama_customer" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-namacustomer">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-form-label">Alamat Delivery</label>
+                        <textarea id="alamat_customer" name="alamat_customer" class="form-control form-active-control"> </textarea>
+                        <div class="invalid-feedback invalid-alamatcustomer">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group" >
+                        <label class="col-form-label">No. HP</label>
+                        <input type="number" id="no_hp_customer"  name="no_hp_customer" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-nohp">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group" >
+                        <label class="col-form-label">Email</label>
+                        <input type="text" id="email_customer"  name="email_customer" class="form-control form-active-control">
+                        <div class="invalid-feedback invalid-email">Data tidak valid</div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-form-label">Catatan</label>
+                        <textarea id="catatan_customer" name="catatan_customer" class="form-control form-active-control"> </textarea>
+                    </div>
+
+
+                    <input type="hidden" id="id_customer" name="id_customer" val="0">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <div class="modal-button-view-only">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary edit">Edit</button>
+                </div>
+                <div class="modal-button-save">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary save">Simpan</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<style>
+    tr:hover{
+        cursor: pointer;
+        background: rgba(20,143,143,0.5);
+        transition: background-color 0.15s ease-in-out;
+    }
+
+</style>
+
+<!-- Page level custom scripts -->
+
+<!-- <script src="<?php echo base_url('assets/js/startbootstrap/demo/datatables-demo.js');?>"></script>-->
+
+<script>
+
+    $('#collapseUser').addClass('show');
+    $('#navbar-user').addClass('active');
+
+    get_customer();
+
+    //get all products
+    function get_customer(){
+        $('.loading').css("display", "block");
+        $('.Veil-non-hover').fadeIn();
+        $.ajax({
+            type        : 'GET', // define the type of HTTP verb we want to use (POST for our form)
+            url         : admin_url + 'get_customer', // the url where we want to POST// our data object
+            dataType    : 'json',
+            success     : function(data){
+                html = '';
+                data.forEach(function(data){
+
+                    html += '<tr>'+
+                        '<td style="display: none;">'+ data.id_customer +'</td>';
+
+                    html += ' <td>'+ data.nama_customer +'</td></tr>';
+                })
+
+                $('#dataTable').DataTable().destroy();
+                $('#main-content').html(html);
+                $('#dataTable').DataTable({
+                    "lengthChange": false
+                });
+
+                $('.loading').css("display", "none");
+                $('.Veil-non-hover').fadeOut();
+            }
+        })
+    }
+
+
+    $('#dataTable').on( 'click', 'tbody tr', function () {
+        $('.loading').css("display", "block");
+        $('.Veil-non-hover').fadeIn();
+        $('body').addClass('modal-open');
+        id_customer = $('#dataTable').DataTable().row( this ).data()[0];
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : admin_url + 'get_customer_by_id', // the url where we want to POST// our data object
+            dataType    : 'json',
+            data        : {id_customer: id_customer},
+            success     : function(data){
+
+                setTimeout(function() {$('.modal-dialog').scrollTop(0);}, 200);
+                $('#id_customer').val(htmlDecode(data.id_customer));
+                $('#nama_customer').val(htmlDecode(data.nama_customer));
+                $('#alamat_customer').val(htmlDecode(data.alamat_customer));
+                $('#no_hp_customer').val(htmlDecode(data.no_hp_customer));
+                $('#email_customer').val(htmlDecode(data.email_customer));
+                $('#catatan_customer').val(htmlDecode(data.catatan_customer));
+
+
+                $('.form-active-control').prop('disabled', true);
+
+                $('.modal-button-save').css('display', 'none');
+                $('.modal-button-view-only').css('display', 'block');
+                $('#customer-modal').modal('toggle');
+
+                $('.loading').css("display", "none");
+                $('.Veil-non-hover').fadeOut();
+            }
+        })
+    });
+
+    $('.add').click(function (e) {
+        e.preventDefault();
+        $('.invalid-feedback').css('display', 'none');
+        $('#id_customer').val(0);
+        setTimeout(function() {$('.modal-dialog').scrollTop(0);}, 200);
+        $('.modal-button-view-only').css('display', 'none');
+        $('#customer-form').trigger('reset');
+        $('.form-active-control').prop('disabled', false);
+        $('#customer-modal').modal('toggle');
+        $('.modal-button-save').css('display', 'block');
+    })
+
+    $('.edit').click(function(e){
+        $('.invalid-feedback').css('display', 'none');
+        setTimeout(function() {$('.modal-dialog').scrollTop(0);}, 200);
+        $('.modal-button-save').css('display', 'block');
+        $('.modal-button-view-only').css('display', 'none');
+        $('.form-active-control').prop('disabled', false);
+    })
+
+    $('.save').click(function(e){
+        $('.loading').css("display", "block");
+        $('.Veil-non-hover').fadeIn();
+        $.ajax({
+            type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url: admin_url + 'add_customer', // the url where we want to POST// our data object
+            dataType: 'json',
+            data: $('#customer-form').serialize(),
+            success: function (response) {
+                $('.invalid-feedback').css('display', 'none');
+                if(response.Status == "OK"){
+                    get_customer();
+                    $('#customer-modal').modal('hide');
+                } else if(response.Status == "FORMERROR") {
+                    response.Error.forEach(function(error){
+                        $('.'+ error +'').css('display', 'block');
+                    })
+                } else if(response.Status == "EXIST") {
+                    show_snackbar('Nama Customer sudah terdaftar');
+                } else if(response.Status == "ERROR" ){
+                    show_snackbar(response.Message);
+                }
+
+                $('.loading').css("display", "none");
+                $('.Veil-non-hover').fadeOut();
+            }
+        })
+    })
+
+
+
+</script>
